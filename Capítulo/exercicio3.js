390b42e0dc9a6109b2f236d2c72e8733a7d4666b
Pedro Barros <devpedrobarros@hotmail.com>
2025-04-15 08:54:05 -0300

    exercicio-3

---
+++ Capítulo/exercicio3.js
@@ -0,0 +1,10 @@
+const pessoa = {
+    nome: 'Goku',
+    equipe: 'Guerreiro Z',
+    seApresentar() {
+      console.log(`Oi, eu sou o ${this.nome}!`);
+    }
+  };
+  
+  pessoa.seApresentar();
+  
