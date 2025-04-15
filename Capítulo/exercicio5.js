1aa4afd59f807affbe1290e8b876e8b4fe04660a
Pedro Barros <devpedrobarros@hotmail.com>
2025-04-15 08:49:44 -0300

    exercicio-5

---
+++ Capítulo/exercicio5.js
@@ -0,0 +1,12 @@
+const equipe = {
+    nome: 'Valentes da Glória',
+    membros: ['Luciano', 'Maria', 'Virgínia', 'Daniela'],
+    imprimeNomes: function() {
+      this.membros.forEach(membro => {
+        console.log(`${membro} é da equipe ${this.nome}`);
+      });
+    }
+  };
+  
+equipe.imprimeNomes();
+  
