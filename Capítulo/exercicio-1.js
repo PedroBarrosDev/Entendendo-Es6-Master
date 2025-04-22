87d1e3acf83babe9ccb6f0a9952abbef0b3c8aed
Pedro Barros <156983487+PedroBarrosDev@users.noreply.github.com>
2025-04-22 10:03:40 -0300

    Revert "exercicio-01"
    This reverts commit a986b7335f2980758f43cd2e8d596cd836a36dfc.

--- Capítulo/exercicio-1.js
+++
@@ -1,7 +0,0 @@
-function somaFaturamento(valores) {
-    return valores.reduce(function(total, valorAtual) {
-        return total + valorAtual;
-    }, 0);
-}
-
-console.log(somaFaturamento([1, 2, 3, 4]));
